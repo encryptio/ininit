ad3f487e6212460a3f312e0f5898131169ea320f
Chris Kastorff <encryptio@gmail.com>
2009-03-24 23:11:09 -0600

    lua/sawtest signal speed update

--- lua/sawtest.lua
+++ lua/sawtest.lua
@@ -1,4 +1,4 @@
-freq = makefn(1, function(x) return x*100+440 end, osc_sine(0, 1));
+freq = makefn(100, function(x) return x*100+440 end, osc_sine(0, 1));
 osc = osc_sawtooth(0, freq)
 
 saver(osc, "audio/sawtest-lua.au")
