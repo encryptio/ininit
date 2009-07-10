b4d8e46a5f56f6f598da82e9bc97c8152c656e5b
Chris Kastorff <encryptio@gmail.com>
2009-07-09 21:30:45 -0400

    License under BSD 3-clause and add README

--- examples/brownian.lua
+++ examples/brownian.lua
@@ -1,3 +1,20 @@
+--[[
+Copyright (c) 2009 Jack Christopher Kastorff
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are
+met:
+
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions, and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * The name Chris Kastorff may not be used to endorse or promote
+      products derived from this software without specific prior written
+      permission.
+]]--
+
 fr = makefn(20, function (x) return x*40 + 100 end, control_brownian(0.0001))
 width = makefn(20, function (x) return (x+1)/2 end, control_brownian(0.0001))
 wave = osc_square(0, fr, width)
